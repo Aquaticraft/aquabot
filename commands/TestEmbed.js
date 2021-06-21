4142ed193d23eda49fa30085096f4a464f579f76
Aquaticraft <86209881+Aquaticraft@users.noreply.github.com>
2021-06-21 09:44:21 -0400

    Update TestEmbed.js

--- commands/TestEmbed.js
+++ commands/TestEmbed.js
@@ -1,1 +1,24 @@
+module.exports = {
+    name: 'embed',
+    description: 'list of commands bot can do',
+    const exampleEmbed = new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addFields(
+		{ name: 'Regular field title', value: 'Some value here' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: 'Inline field title', value: 'Some value here', inline: true },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
 
+channel.send(exampleEmbed);;
+    }
+}
